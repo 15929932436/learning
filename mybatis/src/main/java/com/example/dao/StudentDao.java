e60ebfc548e2025628d0b54934a9d7c9b7fc1f65
15929932436 <442830126@qq.com>
2020-09-15 18:12:41 +0800

    1. 添加insert语句 2. 封装MybatisUtils工具类，使用方法 MyApp2.java

--- mybatis/src/main/java/com/example/dao/StudentDao.java
+++ mybatis/src/main/java/com/example/dao/StudentDao.java
@@ -12,4 +12,12 @@ public interface StudentDao {
      * 查询student表中的所有数据
      */
     public List<Student> selectStudents();
+
+    /**
+     * 插入方法
+     *
+     * @param student， 表示要插入到数据库的数据
+     * @return int, 表示执行insert操作后影响数据库的行数
+     */
+    public int insertStudent(Student student);
 }
