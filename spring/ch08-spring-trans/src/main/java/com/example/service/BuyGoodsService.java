646f45ff414d46f1b96aede22aafca830287ca4c
15929932436 <442830126@qq.com>
2020-09-23 20:25:55 +0800

    spring transaction

---
+++ spring/ch08-spring-trans/src/main/java/com/example/service/BuyGoodsService.java
@@ -0,0 +1,6 @@
+package com.example.service;
+
+public interface BuyGoodsService {
+
+    void buy(Integer goodsId, Integer nums);
+}
---
+++ spring/ch10-spring-trans-aspectj/src/main/java/com/example/dao/SaleDao.java
@@ -0,0 +1,8 @@
+package com.example.dao;
+
+import com.example.domain.Sale;
+
+public interface SaleDao {
+    // 增加销售记录
+    int insertSale(Sale sale);
+}
